1568c7fc64a832d59c560447285b220b680df656
NANGpisitpong <pisitpong04@gmail.com>
2025-06-16 12:46:40 +0700

    Update page.js

--- src/app/contect/page.js
+++ src/app/contect/page.js
@@ -1,7 +1,7 @@
 export default function Contect() {
   return (
     <>
-   <h1 className="text-4xl text-center text-White-500"
+   <h1 className="text-4xl text-center text-White"
    > Contect Page
     </h1>
     </>
