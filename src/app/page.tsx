82cb1b028ff2ed9d3f0a497fc2614645924fa472
NANGpisitpong <pisitpong04@gmail.com>
2025-06-16 12:42:47 +0700

    ol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,7 @@
 export default function Home() {
   return (
     <>
-   <h1 className="text-4xl text-center text-green-500 "
-   > Home Page
-    </h1></>
-    
+   <h1 className="text-4xl text-center text-green-500"> Home Page</h1>
+    </>
   );
 }
