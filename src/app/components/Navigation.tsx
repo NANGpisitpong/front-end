275b455cbb382373d263e5f6f5b59f8f6fe4a42e
SharkyX99 <nuttanawat6933@gmail.com>
2025-06-23 10:36:43 +0700

    Update Navigation.tsx

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -4,10 +4,10 @@ export default function Navigation() {
   return (
     <nav style={{ padding: '1rem', borderBottom: '1px solid #eee' }}>
       <ul style={{ display: 'flex', gap: '1rem', listStyle: 'none' }}>
-       <center> <li><Link href="/">หน้าแรก</Link></li></center>
-       <center> <li><Link href="/about">เกี่ยวกับ</Link></li></center>
-       <center> <li><Link href="/service">บริการ</Link></li></center>
-       <center> <li><Link href="/contect">ติดต่อ</Link></li></center>
+        <li><Link href="/">หน้าแรก</Link></li>
+        <li><Link href="/about">เกี่ยวกับ</Link></li>
+        <li><Link href="/service">บริการ</Link></li>
+        <li><Link href="/contect">ติดต่อ</Link></li>
      </ul>
     </nav>
   );
